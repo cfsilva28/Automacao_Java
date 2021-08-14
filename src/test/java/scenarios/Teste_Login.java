ec7fa2d0f98f456a2636d5705f4675dece020834
Cesar Silva <84938508+cfsilva28@users.noreply.github.com>
2021-08-13 22:08:27 -0300

    Criando Classes Page e Instanciando Chrome Driver

--- src/test/java/scenarios/Teste_Login.java
+++ src/test/java/scenarios/Teste_Login.java
@@ -1,5 +1,33 @@
 package scenarios;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.BeforeClass;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import pages.CadastroPage;
+
 public class Teste_Login {
+
+	static ChromeDriver driver;
+
+	CadastroPage register = new CadastroPage(driver);
+
+	@BeforeClass
+	public static void beforeClass() {
+		driver = setupChromeDriver();
+		driver.get("http://advantageonlineshopping.com/#/");
+	}
+
+	public static ChromeDriver setupChromeDriver() {
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		driver = new ChromeDriver(chromeOptions);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		return driver;
+
+	}
 
 }
