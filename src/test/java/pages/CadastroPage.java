c47ee4cc52ac6f0ea41d628f754254c3b77b5ea5
Cesar Silva <84938508+cfsilva28@users.noreply.github.com>
2021-08-15 23:18:43 -0300

    Inserindo Informacoes Pesoais ao Cadastro

--- src/test/java/pages/CadastroPage.java
+++ src/test/java/pages/CadastroPage.java
@@ -2,16 +2,22 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CadastroPage {
 
 	WebDriver driver;
 
 	public final String CAMPO_REGISTER = "//*[@id=\"menuUser\"]";
+
 	public final String NEW_ACCOUNT = "/html/body/login-modal/div/div/div[3]/a[2]";
 	public final String USER_NAME = "//*[@id=\"formCover\"]/div[1]/div[1]/sec-view[1]/div/input";
+	public final String USER_MAIL = "//*[@id=\"formCover\"]/div[1]/div[1]/sec-view[2]/div/input";
+	public final String USER_PASSWORD = "//*[@id=\"formCover\"]/div[1]/div[2]/sec-view[1]/div/input";
+	public final String USER_PASSWORD_REPPLY = "//*[@id=\"formCover\"]/div[1]/div[2]/sec-view[2]/div/input";
+	public final String USER_NAME_FIRST = "//*[@id=\"formCover\"]/div[2]/div[1]/sec-view[1]/div/input";
+	public final String USER_NAME_LAST = "//*[@id=\"formCover\"]/div[2]/div[1]/sec-view[2]/div/input";
+	public final String USER_NAME_PHONE = "//*[@id=\"formCover\"]/div[2]/div[2]/sec-view/div/input";
+	public final String CREATE = "//a[contains(text(),'CREATE NEW ACCOUNT')]";
 
 	public CadastroPage(WebDriver driver) {
 
@@ -19,16 +25,35 @@ public class CadastroPage {
 	}
 
 	public void AcessoCadastro() {
+
 		driver.findElement(By.xpath(CAMPO_REGISTER)).click();
-		WebDriverWait wait = new WebDriverWait(driver, 20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NEW_ACCOUNT)));
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException timepage) {
+		}
+
 		driver.findElement(By.xpath(NEW_ACCOUNT)).click();
 
 	}
 
-	public void InfoUser() {
-		driver.findElement(By.xpath(USER_NAME)).click();
+	public void Account_Details() {
+
 		driver.findElement(By.xpath(USER_NAME)).sendKeys("cesar2021");
+
+		driver.findElement(By.xpath(USER_MAIL)).sendKeys("softwaretestesqa@gmail.com");
+
+		driver.findElement(By.xpath(USER_PASSWORD)).sendKeys("Brasileiro@2021");
+
+		driver.findElement(By.xpath(USER_PASSWORD_REPPLY)).sendKeys("Brasileiro@2021");
+	}
+
+	public void Personal_Details() {
+
+		driver.findElement(By.xpath(USER_NAME_FIRST)).sendKeys("Cesar");
+
+		driver.findElement(By.xpath(USER_NAME_LAST)).sendKeys("Silva");
+
+		driver.findElement(By.xpath(USER_NAME_PHONE)).sendKeys("011987534907");
 
 	}
 
--- src/test/java/scenarios/Teste_Login.java
+++ src/test/java/scenarios/Teste_Login.java
@@ -6,7 +6,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import pages.CadastroPage;
 
 public class Teste_Login {
@@ -17,17 +16,19 @@ public class Teste_Login {
 
 	@BeforeClass
 	public static void beforeClass() {
-		driver = setupChromeDriver();
 
+		driver = setupChromeDriver();
 		driver.get("http://advantageonlineshopping.com/#/");
 
 	}
 
 	@Test
+
 	public void cadastro() {
 
 		register.AcessoCadastro();
-		register.InfoUser();
+		register.Account_Details();
+		register.Personal_Details();
 
 	}
 
@@ -35,7 +36,7 @@ public class Teste_Login {
 
 		ChromeOptions chromeOptions = new ChromeOptions();
 		driver = new ChromeDriver(chromeOptions);
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		return driver;
 
